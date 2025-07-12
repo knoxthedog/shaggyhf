c98763985c71bb6d3536ddf5a60b803bcc4020cb
David Dalley <ddalley+knox@gmail.com>
2025-07-11 19:13:30 -0700

    add rw_matcher to vite build

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,6 +3,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/shaggyhf/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        rw_matcher: 'rw_matcher.html',
+      }
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
